7f54c9cef613e3fa58b1c394b23b7fc872d30e31
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-06 23:09:21 +0530

    chapter 4

--- Module1.java
+++ Module1.java
@@ -120,3 +120,46 @@ class Loading {
 // chapter 3 Finished.
 
 //chapter 4:
+//Defining Attributes
+// A class has attributes and methods. The attributes are basically variables within a class.
+// Let's create a class called Vehicle, with its corresponding attributes and methods.
+
+public class Vehicle {
+  int maxSpeed;
+  int wheels;
+  String color;
+  double fuelCapacity;  
+
+  void horn() {
+    System.out.println("Beep!");
+  }  
+}
+//maxSpeed, wheels, color, and fuelCapacity are the attributes of our Vehicle class, and horn() is the only method.You can define as many attributes and methods as necessary.
+
+// Creating Objects
+
+// Next, we can create multiple objects of our Vehicle class, and use the dot syntax to access their attributes and methods.
+
+public class Vehicle {
+    int maxSpeed;
+    int wheels;
+    String color;
+    double fuelCapacity;  
+    
+    void horn() {
+        System.out.println("Beep!");
+    }
+}
+
+class MyClass {
+    public static void main(String[ ] args) {
+        Vehicle v1 = new Vehicle();
+        Vehicle v2 = new Vehicle();
+        v1.color = "red";
+        v2.horn();
+    }
+}
+
+// Run the code and see how it works!
+
+// chapter 4 Finished.
