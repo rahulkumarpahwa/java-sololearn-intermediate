fffe0e5ef06d1b1d8c6402cf86af490088d691bc
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-08 22:53:25 +0530

    chapter 11

--- Module1.java
+++ Module1.java
@@ -596,3 +596,103 @@ public class Program {
 // Java automatically provides a default constructor, so all classes have a constructor, whether one is specifically defined or not.
 
 // chapter 10 Finished.
+
+// chapter 11 :
+// PRACTICE EXERCISE
+// Constructors
+
+// Your friend is a cashier at a movie theater. He knows that you are an awesome java developer so he asked you to help him out and create a program that gets movie title, row, and seat information and prints out a new ticket.
+// Complete the existing code by adding a constructor to Ticket class so that it can be correctly initialized.
+
+// Sample Input
+// Jaws
+// 5
+// 1
+
+// Sample Output
+// Movie: Jaws
+// Row: 5
+// Seat: 1
+// You can figure out the constructor parameters by looking at the types of data that is being inputted.
+
+import java.util.Scanner;
+
+class Main {
+   public static void main(String[] args) {
+       Scanner read = new Scanner(System.in);
+       String movie = read.nextLine();
+       int row = read.nextInt();
+       int seat = read.nextInt();
+       Ticket ticket = new Ticket(movie, row, seat);
+       System.out.println("Movie: " + ticket.getMovie());
+       System.out.println("Row: " + ticket.getRow());
+       System.out.println("Seat: " + ticket.getSeat());
+   }
+}
+
+class Ticket {
+   private String movie;
+   private int row;
+   private int seat;
+
+   //complete the constructor
+   public Ticket() {
+       
+   }
+
+   public String getMovie() {
+       return movie;
+   }
+
+   public int getRow() {
+       return row;
+   }
+
+   public int getSeat() {
+       return seat;
+   }
+}
+
+// My Solution:
+
+import java.util.Scanner;
+
+class Main {
+   public static void main(String[] args) {
+       Scanner read = new Scanner(System.in);
+       String movie = read.nextLine();
+       int row = read.nextInt();
+       int seat = read.nextInt();
+       Ticket ticket = new Ticket(movie, row, seat);
+       System.out.println("Movie: " + ticket.getMovie());
+       System.out.println("Row: " + ticket.getRow());
+       System.out.println("Seat: " + ticket.getSeat());
+   }
+}
+
+class Ticket {
+   private String movie;
+   private int row;
+   private int seat;
+
+   public Ticket(String movie, int row, int seat) {
+       this.movie = movie;
+       this.row = row;
+       this.seat = seat;
+       
+   }
+
+   public String getMovie() {
+       return movie;
+   }
+
+   public int getRow() {
+       return row;
+   }
+
+   public int getSeat() {
+       return seat;
+   }
+}
+
+// chapter 11 Finished.
