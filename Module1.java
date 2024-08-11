412dcbe6d4622fd0411772d3d3a08b7c015ed955
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-12 02:07:04 +0530

    chapter 16

--- Module1.java
+++ Module1.java
@@ -939,3 +939,25 @@ public class MyClass {
 // Also, the main method must always be static.
 
 // Chapter 15 Finished.
+
+// chapter 16:
+
+// final
+
+// Use the final keyword to mark a variable constant, so that it can be assigned only once.
+
+// Example:
+
+class MyClass {
+    public static final double PI = 3.14; 
+    public static void main(String[ ] args) {
+        System.out.println(PI);
+    }
+}
+// PI is now a constant. Any attempt to assign it a value will cause an error.
+
+// Methods and classes can also be marked final. This serves to restrict methods so that they can't be overridden and classes so that they can't be subclassed.
+
+// These concepts will be covered in the next module.
+
+// chapter 16 Finished.
