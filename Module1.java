d386d2e113c085b36ee7ead4a235713eb78697bc
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-12 09:23:03 +0530

    chapter 17

--- Module1.java
+++ Module1.java
@@ -961,3 +961,49 @@ class MyClass {
 // These concepts will be covered in the next module.
 
 // chapter 16 Finished.
+
+// chapter 17:
+// Packages
+
+// Packages are used to avoid name conflicts and to control access to classes.
+
+// A package can be defined as a group made up of similar types of classes, along with sub-packages.
+
+// Creating a package in Java is quite easy. Simply right click on your src directory and click New->Package. Give your package a name and click Finish.
+
+// You will notice that the new package appears in the project directory. Now you can move and create classes inside that package. We have moved our Vehicle, Counter and Animal classes to the package samples.
+
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+String imagePath = "./image3.jpg";
+BufferedImage myPicture = ImageIO.read(new File(imagePath));
+
+// When you move/create a class in your package, the following code will appear at the top of the list of files.
+package samples;
+
+// This indicates the package to which the class belongs.
+
+// Now, we need to import the classes that are inside a package in our main to be able to use them.
+
+// The following example shows how to use the Vehicle class of the samples package.
+
+import samples.Vehicle;
+
+class MyClass {
+  public static void main(String[ ] args) {
+    Vehicle v1 = new Vehicle();
+    v1.horn();
+  }
+}
+
+// Two major results occur when a class is placed in a package. First, the name of the package becomes a part of the name of the class. Second, the name of the package must match the directory structure where the corresponding class file resides.
+
+// Use a wildcard to import all classes in a package.
+
+// For example, import samples.* will import all classes in the samples package.
+
+// Your answer is correct because there is no specific limit to the number of packages that can be contained in a Java program. You can organize your code into as many packages as needed to maintain a clear structure and organization.
+
+// chapter 17 Finished.
