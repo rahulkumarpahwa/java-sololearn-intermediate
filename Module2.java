0daf7c906f0876050df409a86ae88f0c4316f3ae
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-13 20:27:23 +0530

    chapter 6

--- Module2.java
+++ Module2.java
@@ -373,3 +373,95 @@ class Program {
 // This is very useful when you have multiple subclasses of the superclass.
 
 // chapter 5 Finished.
+
+// chapter 6: 
+// PRACTICE EXERCISE
+// Polymorphism
+
+// Vehicles can be classified according to what energy source powers them. The program you are given has 3 vehicle classes: Vehicle, ElectricVehicle and HybridVehicle.
+
+// ElectricVehicle and HybridVehicle classes are inherited from Vehicle class.
+
+// Complete the program by reimplementing method resource() in inherited classes, so that the given calls work correctly.
+
+// The subclass can implement a parent class method based on its requirement.
+
+class Main {
+   public static void main(String[] args) {
+       
+       Vehicle vehicle = new Vehicle();
+       Vehicle electric = new ElVehicle();
+       Vehicle hybrid = new HybridVehicle();
+       
+       //calls
+       vehicle.start();
+       vehicle.resource();
+       electric.start();
+       electric.resource();
+       hybrid.start();
+       hybrid.resource();
+   }
+}
+class Vehicle{
+    public void start(){
+        System.out.println("Starting");
+    }
+    public void resource(){
+        System.out.println("I use petrol");
+    }
+}
+
+class ElVehicle extends Vehicle{
+    /*reimplement resource() method 
+    to output "I use electricity"*/
+    
+}
+
+class HybridVehicle extends Vehicle{
+    /*reimplement resource() method 
+    to output "I use both petrol and electricity"*/
+    
+}
+
+
+// my solution :
+
+class Main {
+   public static void main(String[] args) {
+       
+       Vehicle vehicle = new Vehicle();
+       Vehicle electric = new ElVehicle();
+       Vehicle hybrid = new HybridVehicle();
+       
+       //calls
+       vehicle.start();
+       vehicle.resource();
+       electric.start();
+       electric.resource();
+       hybrid.start();
+       hybrid.resource();
+   }
+}
+class Vehicle{
+    public void start(){
+        System.out.println("Starting");
+    }
+    public void resource(){
+        System.out.println("I use petrol");
+    }
+}
+
+class ElVehicle extends Vehicle{
+    public void resource(){
+        System.out.println("I use electricity");
+    }
+    
+}
+
+class HybridVehicle extends Vehicle{
+    public void resource(){
+        System.out.println("I use both petrol and electricity");
+    }    
+}
+
+// chapter 6 Finished.
