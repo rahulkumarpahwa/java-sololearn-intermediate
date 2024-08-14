0dce6ec1505c7d9652d96982dba4b9768ff90a57
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-14 22:41:12 +0530

    chapter 12

--- Module2.java
+++ Module2.java
@@ -895,3 +895,124 @@ public class Program {
 // When you implement an interface, you need to override all of its methods.
 
 // chapter 11 Finished.
+
+// chapter 12:
+
+// PRACTICE EXERCISE
+// Interfaces
+
+// You love all animals, and have a dog and a cat as pets.
+
+// The program you are given defines an abstract class Animal, and has Dog & Cat classes inherited from it.
+
+// Implement the Swimmer and Player interface and override their methods swim() and play(), so that the given method calls output the following messages:
+
+// swim():
+
+// Dog => "Dog is swimming"
+
+// Cat => "Cat is swimming"
+
+// play():
+
+// Dog => "Dog is playing"
+
+// Cat => "Cat is playing"
+
+// You can implement multiple interfaces - by writing their names alternately separated by commas.
+
+// Implement the Swimmer and Player interfaces only for Animal class, then override the swim() and play() methods in inherited classes.
+
+
+class Main {
+   public static void main(String[] args) {
+       Animal dog = new Dog();
+       Animal cat = new Cat();
+       
+       dog.swim();
+       dog.play();
+       cat.swim();
+       cat.play();
+
+   }
+}
+
+interface Swimmer {
+   void swim();
+}
+
+interface Player {
+   void play();
+}
+
+//implement the Swimmer and the Player interfaces
+abstract class Animal{
+}
+
+
+class Dog extends Animal {
+    //Override the swim() and the play() methods
+    
+   }
+
+
+class Cat extends Animal {
+    //Override the swim() and the play() methods
+    
+}
+
+// my Solution:
+
+class Main {
+   public static void main(String[] args) {
+       Animal dog = new Dog();
+       Animal cat = new Cat();
+       
+       dog.swim();
+       dog.play();
+       cat.swim();
+       cat.play();
+
+   }
+}
+
+interface Swimmer {
+   void swim();
+}
+
+interface Player {
+   void play();
+}
+
+//implement the Swimmer and the Player interfaces
+abstract class Animal implements Swimmer, Player {
+}
+
+
+class Dog extends Animal {
+    public void swim(){
+    System.out.println("Dog is swimming");
+
+    }
+
+    public void play(){
+     System.out.println("Dog is playing");  
+    }
+    
+}
+
+class Cat extends Animal {
+    //Override the swim() and the play() methods
+    public void swim(){
+    System.out.println("Cat is swimming");
+
+    }
+
+    public void play(){
+     System.out.println("Cat is playing");  
+    }
+     
+   
+}
+
+// chapter 12 Finished.
