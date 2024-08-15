62da053c6d3c02c0d388c2cd4433d0bb684c6b59
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-15 23:57:09 +0530

    chapter 13

--- Module2.java
+++ Module2.java
@@ -1016,3 +1016,39 @@ class Cat extends Animal {
 }
 
 // chapter 12 Finished.
+
+// chapter 13: 
+
+// Type Casting
+
+// Assigning a value of one type to a variable of another type is known as Type Casting.
+
+// To cast a value to a specific type, place the type in parentheses and position it in front of the value.
+
+// Example:
+
+public class Program {
+    public static void main(String[] args) {
+        double a = 42.571;
+        int b = (int) a;
+        System.out.println(b);
+    }
+}
+
+// The code above is casting the value 3.14 to an integer, with 3 as the resulting value.
+
+// Another example:
+
+public class Program {
+    public static void main(String[] args) {
+        double a = 42.571;
+        int b = (int) a;
+        System.out.println(b);
+    }
+}
+
+// Java supports automatic type casting of integers to floating points, since there is no loss of precision.
+
+// On the other hand, type casting is mandatory when assigning floating point values to integer variables.
+
+// chapter 13 Finished.
