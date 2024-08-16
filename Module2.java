7fe84796c65b532cf32ce458a2c7efc8193f54c9
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-16 23:58:58 +0530

    chapter 18

--- Module2.java
+++ Module2.java
@@ -1261,3 +1261,39 @@ class Purchase {
 }
 
 // chapter 17 Finished.
+
+// chapter 18:
+
+// Inner Classes
+
+// Java supports nesting classes; a class can be a member of another class.
+
+// Creating an inner class is quite simple. Just write a class within a class. Unlike a class, an inner class can be private. Once you declare an inner class private, it cannot be accessed from an object outside the class.
+
+// Example:
+
+class Robot {
+    int id;
+    Robot(int i) {
+        id = i;
+        Brain b = new Brain();
+        b.think();
+    }
+
+    private class Brain {
+        public void think() {
+            System.out.println(id + " is thinking");
+        }
+    }
+}
+
+public class Program {
+    public static void main(String[] args) {
+        Robot r = new Robot(1);
+    }
+}
+
+
+// The class Robot has an inner class Brain. The inner class can access all of the member variables and methods of its outer class, but it cannot be accessed from any outside class.
+
+// chapter 18 Finished.
