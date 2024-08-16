bbd215cf784e125eba834a1a091ea4fb9f2ac572
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-16 23:40:12 +0530

    chapter 16

--- Module2.java
+++ Module2.java
@@ -1130,3 +1130,68 @@ Animal a = new Cat();
 // Why is upcasting automatic, downcasting manual? Well, upcasting can never fail. But if you have a group of different Animals and want to downcast them all to a Cat, then there's a chance that some of these Animals are actually Dogs, so the process fails.
 
 // chapter 15 Finished.
+
+// chapter 16:
+
+// Anonymous Classes
+
+// Anonymous classes are a way to extend the existing classes on the fly.
+
+// For example, consider having a class Machine:
+
+class Machine {
+  public void start() {
+    System.out.println("Starting...");
+  }
+}
+
+// When creating the Machine object, we can change the start method on the fly.
+
+class Machine {
+    public void start() {
+        System.out.println("Starting...");
+    }
+}
+
+class Program {
+    public static void main(String[ ] args) {
+        Machine m = new Machine() {
+            @Override public void start() {
+                System.out.println("Wooooo");
+            }
+        };
+        m.start();
+    }
+}
+
+// After the constructor call, we have opened the curly braces and have overridden the start method's implementation on the fly.
+
+// The @Override annotation is used to make your code easier to understand, because it makes it more obvious when methods are overridden.
+
+// Anonymous Classes
+
+// The modification is applicable only to the current object, and not the class itself. So if we create another object of that class, the start method's implementation will be the one defined in the class.
+
+class Machine {
+    public void start() {
+        System.out.println("Starting...");
+    }
+}
+
+class Program {
+    public static void main(String[ ] args) {
+        Machine m1 = new Machine() {
+            @Override public void start() {
+                System.out.println("Wooooo");
+            }
+        };
+        Machine m2 = new Machine();
+        m2.start();
+    }
+}
+
+// output :
+
+// Starting...
+
+// chapter 16 Finished.
