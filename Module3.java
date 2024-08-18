6743e559904031d0831cdc6db6abfaf6a4ab5fec
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-19 00:27:52 +0530

    chapter 2

--- Module3.java
+++ Module3.java
@@ -55,3 +55,67 @@ public class MyClass {
 // Notice the (Exception e) statement in the catch block - it is used to catch all possible Exceptions.
 
 // chapter 1 Finished.
+
+// chapter 2: 
+
+// PRACTICE EXERCISE
+// Exception Handling
+
+// An online electronics store has 5 product categories - PCs, Notebooks, Tablets, Phones, and Аccessories.
+
+// The program you are given declares an array and stores that types into it.
+
+// Write a program to take N number as input and output the element of the array with N index. If the index is out of range, program should output "Wrong Option".
+
+// Sample Input
+
+// 5
+
+// Sample Output
+
+// Wrong Option
+
+// Use the Exception type to catch all possible exceptions.
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner scanner = new Scanner(System.in);
+	    int choice = scanner.nextInt();
+	    
+	    String[] categories = {"PCs", "Notebooks", "Tablets", "Phones", "Аccessories"};
+	    
+	    //complete the code
+	    try {
+	        
+	    } catch() {
+	        
+	    }
+	}
+}
+
+// my solution :
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner scanner = new Scanner(System.in);
+	    int choice = scanner.nextInt();
+	    
+	    String[] categories = {"PCs", "Notebooks", "Tablets", "Phones", "Аccessories"};
+	    
+	    //complete the code
+	    try {
+	        System.out.println(categories[choice]);
+	    } catch(Exception e) {
+	        System.out.println("Wrong Option");
+	    }
+	}
+}
+
+// chapter 2 Finished.
