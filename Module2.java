26d7f17fec7a650e41c0c3cd03fca225a0ba460c
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-18 23:06:44 +0530

    chapter 22

--- Module2.java
+++ Module2.java
@@ -1584,3 +1584,27 @@ public class Player{
 }
 
 // chapter 21 Finished.
+
+// chapter 22:
+
+// Java API
+
+// The Java API is a collection of classes and interfaces that have been written for you to use.
+
+// The Java API Documentation with all of the available APIs can be located on the Oracle website at 
+
+// http://docs.oracle.com/javase/7/docs/api/
+
+// Once you locate the package you want to use, you need to import it into your code.
+
+// The package can be imported using the import keyword.
+
+// For example:
+
+import java.awt.*;
+
+// The awt package contains all of the classes for creating user interfaces and for painting graphics and images.
+
+// The wildcard character (*) is used to import all of the classes in the package.
+
+// chapter 22 Finished.
