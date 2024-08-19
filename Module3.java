6c2f241fc6066443943770c887a9713bbeed7f82
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-19 23:03:36 +0530

    chapter 3

--- Module3.java
+++ Module3.java
@@ -119,3 +119,57 @@ public class Main
 }
 
 // chapter 2 Finished.
+
+// chapter 3: 
+
+// throw
+
+// The <b>throw </b>keyword allows you to manually generate exceptions from your methods. Some of the numerous available exception types include the IndexOutOfBoundsException, IllegalArgumentException, ArithmeticException, and so on.
+
+// For example, we can throw an ArithmeticException in our method when the parameter is 0.
+
+public class Program {
+
+    static int div(int a, int b) throws ArithmeticException {
+        if(b == 0) {
+            throw new ArithmeticException("Division by Zero");
+        } else {
+            return a / b;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(div(42, 0));
+    }
+
+}
+
+// The <b>throws </b>statement in the method definition defines the type of Exception(s) the method can throw.
+
+// Next, the <b>throw </b>keyword throws the corresponding exception, along with a custom message.
+
+// If we call the div method with the second parameter equal to 0, it will throw an ArithmeticException with the message "Division by Zero".
+
+// Multiple exceptions can be defined in the throws statement using a comma-separated list.
+
+// Exception Handling
+
+// A single try block can contain multiple catch blocks that handle different exceptions separately.
+
+// Example:
+
+try {
+  //some code
+} catch (ExceptionType1 e1) {
+  //Catch block
+} catch (ExceptionType2 e2) {
+  //Catch block
+} catch (ExceptionType3 e3) {
+  //Catch block
+} 
+
+// All catch blocks should be ordered from most specific to most general.
+
+// Following the specific exceptions, you can use the Exception type to handle all other exceptions as the last catch.
+
+// chapter 3 Finished.
