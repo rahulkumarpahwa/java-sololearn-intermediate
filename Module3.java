a31b75f99cc181ba5b10b98380d99b6754e1e00c
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-24 08:40:33 +0530

    chapter 10

--- Module3.java
+++ Module3.java
@@ -611,3 +611,78 @@ public class Main {
 }
 
 // chapter 9 Finished.
+
+// chapter 10:
+
+// LinkedList
+
+// The LinkedList is very similar in syntax to the ArrayList.
+
+// You can easily change an ArrayList to a LinkedList by changing the object type.
+
+import java.util.LinkedList;
+
+public class MyClass {
+    public static void main(String[ ] args) {
+        LinkedList<String> c = new LinkedList<String>();
+        c.add("Red");
+        c.add("Blue");
+        c.add("Green");
+        c.add("Orange");
+        c.remove("Green");
+        System.out.println(c);
+    }
+}
+
+//Note :  You cannot specify an initial capacity for the LinkedList.
+
+// LinkedList vs. ArrayList
+
+// The most notable difference between the LinkedList and the ArrayList is in the way they store objects.
+
+// The ArrayList is better for storing and accessing data, as it is very similar to a normal array.
+
+// The LinkedList is better for manipulating data, such as making numerous inserts and deletes.
+
+// In addition to storing the object, the LinkedList stores the memory address (or link) of the element that follows it. It's called a LinkedList because each element contains a link to the neighboring element.
+
+// image 7 check:
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+String imagePath = "./image7.jpg";
+BufferedImage myPicture = ImageIO.read(new File(imagePath));
+
+
+// You can use the enhanced for-loop to iterate over its elements.
+
+import java.util.LinkedList;
+
+public class MyClass {
+    public static void main(String[ ] args) {
+        LinkedList<String> c = new LinkedList<String>();
+        c.add("Red");
+        c.add("Blue");
+        c.add("Green");
+        c.add("Orange");
+        c.remove("Green");
+        
+        for(String s: c) {
+            System.out.println(s);
+        }
+    }
+}
+
+//output: 
+// Red
+// Blue
+// Orange 
+
+// Summary:
+
+// - Use an ArrayList when you need rapid access to your data.
+
+// - Use a LinkedList when you need to make a large number of inserts and/or deletes.
+
+// chapter 10 Finished.
