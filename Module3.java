8cd74076be9ad49eb57b170367659c0bd1cd20f7
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-24 08:21:10 +0530

    chapter 7

--- Module3.java
+++ Module3.java
@@ -431,3 +431,48 @@ class Name extends Thread{
 }
 
 // chapter 6 Finished.
+
+// chapter 7:
+
+// Types of Exceptions
+
+// There are two exception types, checked and unchecked (also called runtime). The main difference is that checked exceptions are checked when compiled, while unchecked exceptions are checked at runtime.
+
+// As mentioned in our previous lesson, Thread.sleep() throws an InterruptedException. This is an example of a checked exception. Your code will not compile until you've handled the exception.
+
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+String imagePath = "./image6.jpg";
+BufferedImage myPicture = ImageIO.read(new File(imagePath));
+
+
+public class MyClass {
+  public static void main(String[ ] args) {
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+      //some code
+    }
+  }
+}
+
+// We have seen examples of unchecked exceptions, which are checked at runtime, in previous lessons.
+
+// Example (when attempting to divide by 0):
+
+public class MyClass {
+    public static void main(String[ ] args) {
+        int value = 7;
+        value = value / 0;
+    }
+}
+
+// OUTPUT
+
+// Exception in thread "main" java.lang.ArithmeticException: / by zero 	at MyClass.main(MyClass.java:5)
+
+// It is good to know the Types of Exceptions because they can help you debug your code faster.
+
+// chapter 7 Finished.
