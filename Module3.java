29d7ecc239cee863f785786c7a46e5c87f0d67d6
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-24 08:32:15 +0530

    chapter 9

--- Module3.java
+++ Module3.java
@@ -537,3 +537,77 @@ public class MyClass {
 // Note: As with arrays, the indexing starts with 0.
 
 // chapter 8 Finished.
+
+// chapter 9: 
+// PRACTICE EXERCISE
+// ArrayList
+
+// The program you are given declares an even nums ArrayList.
+
+// Write a program to take numbers as input and add them an ArrayList while the size of the ArrayList isn't equal to 3.
+
+// Then calculate and output the average of all values in integers.
+
+// Sample Input
+
+// 5
+
+// 2
+
+// 4
+
+// Sample Output
+
+// 3
+
+// Use <b>evennums.size()</b> in while loop condition.
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[ ] args) {
+	    Scanner scanner = new Scanner(System.in);
+        
+        ArrayList<Integer> evennums = new ArrayList<Integer>();
+        
+        while(evennums.size()<3){
+            int num = scanner.nextInt();
+            //your code goes here
+            
+        }
+        
+        //calculate and output the average integer value
+        
+    }
+}
+
+// my solution :
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[ ] args) {
+	    Scanner scanner = new Scanner(System.in);
+        
+        ArrayList<Integer> evennums = new ArrayList<Integer>();
+        
+        while(evennums.size()<3){
+            int num1 = scanner.nextInt();
+            int num2 = scanner.nextInt();
+            int num3 = scanner.nextInt();
+
+            evennums.add(num1);
+            evennums.add(num2);
+            evennums.add(num3);
+        }
+        
+        int avg = (evennums.get(0) + evennums.get(1) + evennums.get(2))/3;
+        System.out.println(avg);
+
+        
+    }
+}
+
+// chapter 9 Finished.
