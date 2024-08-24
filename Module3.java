3f28bd5c53e6a7638f850e62f2238be884d32b04
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-24 08:24:25 +0530

    chapter 8

--- Module3.java
+++ Module3.java
@@ -476,3 +476,64 @@ public class MyClass {
 // It is good to know the Types of Exceptions because they can help you debug your code faster.
 
 // chapter 7 Finished.
+
+// chapter 8:
+// ArrayList
+
+// The Java API provides special classes to store and manipulate groups of objects.
+
+// One such class is the ArrayList. Standard Java arrays are of a fixed length, which means that after they are created, they cannot expand or shrink.
+
+// On the other hand, ArrayLists are created with an initial size, but when this size is exceeded, the collection is automatically enlarged.
+
+// When objects are removed, the ArrayList may shrink in size. Note that the ArrayList class is in the java.util package, so it's necessary to import it before using it.
+
+// Create an ArrayList as you would any object.
+
+import java.util.ArrayList;
+//...
+ArrayList colors = new ArrayList();
+
+// You can optionally specify a capacity and type of objects the ArrayList will hold:
+
+ArrayList<String> colors = new ArrayList<String>(10);
+
+// The code above defines an ArrayList of Strings with 10 as its initial size.
+// ArrayLists store objects. Thus, the type specified must be a class type. You cannot pass, for example, int as the objects' type. Instead, use the special class types that correspond to the desired value type, such as Integer for int, Double for double, and so on.
+
+// ArrayList
+
+// The ArrayList class provides a number of useful methods for manipulating its objects.
+
+// The <b>add()</b> method adds new objects to the ArrayList. Conversely, the remove() method removes objects from the ArrayList.
+
+// Example:
+
+import java.util.ArrayList;
+
+public class MyClass {
+    public static void main(String[ ] args) {
+        ArrayList<String> colors = new ArrayList<String>();
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Orange");
+        colors.remove("Green");
+        
+        System.out.println(colors);
+    }
+}
+
+// Other useful methods include the following:
+
+// - <b>contains()</b>: Returns true if the list contains the specified element
+
+// - <b>get(int index)</b>: Returns the element at the specified position in the list
+
+// - <b>size()</b>: Returns the number of elements in the list
+
+// - <b>clear()</b>: Removes all of the elements from the list
+
+// Note: As with arrays, the indexing starts with 0.
+
+// chapter 8 Finished.
