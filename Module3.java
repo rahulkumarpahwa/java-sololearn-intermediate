c7af77631b24145cdbf62a8cb788e9dbb209169a
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-26 00:44:49 +0530

    chapter 17

--- Module3.java
+++ Module3.java
@@ -1095,3 +1095,82 @@ public class Main {
 }
 
 // chapter 16 Finished.
+
+// chapter 17:
+
+// Iterators
+
+// An Iterator is an object that enables to cycle through a collection, obtain or remove elements. 
+
+// Before you can access a collection through an iterator, you must obtain one. Each of the collection classes provides an <b>iterator()</b> method that returns an iterator to the start of the collection. By using this iterator object, you can access each element in the collection, one element at a time.
+
+// The Iterator class provides the following methods:
+
+// <b>hasNext()</b>: Returns true if there is at least one more element; otherwise, it returns false.
+
+// <b>next()</b>: Returns the next object and advances the iterator.
+
+// <b>remove()</b>: Removes the last object that was returned by next from the collection.
+
+// The Iterator class must be imported from the java.util package.
+
+// Example:
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class MyClass {
+    public static void main(String[ ] args) {
+        LinkedList<String> animals = new LinkedList<String>();
+        animals.add("fox");
+        animals.add("cat");
+        animals.add("dog");
+        animals.add("rabbit");
+        
+        Iterator<String> it = animals.iterator();
+        String value = it.next();
+        System.out.println(value);
+    }
+}
+// output:
+// fox
+
+// <b>it.next()</b> returns the first element in the list and then moves the iterator on to the next element.
+
+// Each time you call <b>it.next()</b>, the iterator moves to the next element of the list.
+
+// Iterators
+
+// Typically, iterators are used in loops. At each iteration of the loop, you can access the corresponding list element.
+
+// Example:
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class MyClass {
+    public static void main(String[ ] args) {
+        LinkedList<String> animals = new LinkedList<String>();
+        animals.add("fox");
+        animals.add("cat");
+        animals.add("dog");
+        animals.add("rabbit");
+        
+        Iterator<String> it = animals.iterator();
+        while(it.hasNext()) {
+            String value = it.next();
+            System.out.println(value);   
+        }
+    }
+}
+
+// output:
+// fox
+// cat
+// dog
+// rabbit
+
+
+// Here, the while loop determines whether the iterator has additional elements, prints the value of the element, and advances the iterator to the next.
+
+// chapter 17 Finished.
