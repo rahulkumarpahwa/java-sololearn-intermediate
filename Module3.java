5fac193648049943a3e6673e48f5deb8ab90d9f6
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-25 23:08:38 +0530

    chapter 13 and last commit was 12 not 11th

--- Module3.java
+++ Module3.java
@@ -819,3 +819,98 @@ public class MyClass {
 // null is a special type that represents the absence of a value.
 
 // chapter 12 Finished.
+
+// chapter 13:
+
+// PRACTICE EXERCISE
+// HashMap
+
+// The program you are given defines and outputs HashMap, where the names of employees are stored as keys, and their ages as values.
+
+// It also takes N number from user as age limit. Write code to delete all the employees whom age is less than N number.
+
+// The line of code for the output of HashMap object is already provided.
+
+// Sample Input
+
+// 25
+
+// Sample Output
+
+// {Robert=32, John=28}
+
+// Hint
+
+// Use get() method to access values in the HashMap and remove() to delete them.
+
+// nameArr is an array of given HashMap keys and created for iteration.
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[ ] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        HashMap<String, Integer> ages = new HashMap<String, Integer>();
+        ages.put("David", 22);
+        ages.put("Tom", 23);
+        ages.put("Robert", 32);
+        ages.put("Alice", 21);
+        ages.put("Sophie", 19);
+        ages.put("Maria", 24);
+        ages.put("John", 28);
+        
+        
+        String[] nameArr = new String[ages.size()];
+        nameArr = ages.keySet().toArray(nameArr);
+        
+        int ageLimit = scanner.nextInt();
+        
+        for (String emp : nameArr){
+            //your code goes here
+            
+        }
+        
+        System.out.println(ages);
+    }
+}
+
+//my solution :
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[ ] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        HashMap<String, Integer> ages = new HashMap<String, Integer>();
+        ages.put("David", 22);
+        ages.put("Tom", 23);
+        ages.put("Robert", 32);
+        ages.put("Alice", 21);
+        ages.put("Sophie", 19);
+        ages.put("Maria", 24);
+        ages.put("John", 28);
+        
+        
+        String[] nameArr = new String[ages.size()];
+        nameArr = ages.keySet().toArray(nameArr);
+        
+        int ageLimit = scanner.nextInt();
+        
+        for (String emp : nameArr){
+            if( ages.get(emp) < ageLimit ){
+                ages.remove(emp);
+            }
+            
+        }
+        
+        System.out.println(ages);
+    }
+}
+
+
+// chapter 13 Finished.
