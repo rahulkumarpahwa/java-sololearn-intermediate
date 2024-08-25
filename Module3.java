868a5d0ec6504faa8a1717d55a59e83673aac75f
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-25 23:36:26 +0530

    chapter 14

--- Module3.java
+++ Module3.java
@@ -914,3 +914,42 @@ public class Main {
 
 
 // chapter 13 Finished.
+
+// chapter 14:
+
+// Sets
+
+// A Set is a collection that cannot contain duplicate elements. It models the mathematical set abstraction. (means when we say a data structure "models the mathematical set abstraction," it means it's designed to work with collections of unique items, just like a mathematical set.)
+
+// One of the implementations of the Set is the HashSet class.
+
+// Example:
+
+import java.util.HashSet;
+
+public class MyClass {
+    public static void main(String[ ] args) {
+        HashSet<String> set = new HashSet<String>();
+        set.add("A");
+        set.add("B");
+        set.add("C");
+        System.out.println(set);
+    }
+}
+
+// You can use the <b>size()</b> method to get the number of elements in the HashSet.
+
+// LinkedHashSet
+
+// The HashSet class does not automatically retain the order of the elements as they're added. To order the elements, use a LinkedHashSet, which maintains a linked list of the set's elements in the order in which they were inserted.
+
+// What is hashing?
+
+// A hash table stores information through a mechanism called hashing, in which a key's informational content is used to determine a unique value called a hash code.
+
+// So, basically, each element in the HashSet is associated with its unique hash code.
+
+// You've learned about the various collection types that are available in Java, including Lists, Maps, and Sets. The choice of which one to use is specific to the data you need to store and manipulate.
+
+
+// chapter 14 Finished.
