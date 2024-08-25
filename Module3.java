985ac4c6f13f3c0ba67b0319e2281f159a17b159
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-26 00:53:51 +0530

    chapter 18

--- Module3.java
+++ Module3.java
@@ -1174,3 +1174,84 @@ public class MyClass {
 // Here, the while loop determines whether the iterator has additional elements, prints the value of the element, and advances the iterator to the next.
 
 // chapter 17 Finished.
+
+// chapter 18:
+// PRACTICE EXERCISE
+// Iterators
+
+// The program you are given declares a LinkedList of integers, takes 5 numbers from user, and add them to it.
+
+// Write a program to calculate and output the sum of them elements.
+
+// Sample Input
+
+// 4
+
+// 3
+
+// 50
+
+// 3
+
+// 8
+
+// Sample Output
+
+// 68
+
+// Use Iterator and its methods to execute the calculation using the while loop.
+
+
+import java.util.Scanner;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[ ] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        LinkedList<Integer> nums = new LinkedList<Integer>();
+        
+        while(nums.size()<5){
+            int num = scanner.nextInt();
+            nums.add(num);
+        }
+        
+        
+        int sum = 0;
+        //your code goes here
+        
+        
+        System.out.println(sum);
+    }
+}
+
+//my solution :
+
+import java.util.Scanner;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[ ] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        LinkedList<Integer> nums = new LinkedList<Integer>();
+        
+        while(nums.size()<5){
+            int num = scanner.nextInt();
+            nums.add(num);
+        }
+        
+        
+        int sum = 0;
+        Iterator<Integer> it = nums.iterator();
+
+        while(it.hasNext()) {
+              sum += it.next();
+        }
+        System.out.println(sum);
+    }
+}
+
+// chapter 18 Finished.
